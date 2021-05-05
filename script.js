5c410ddd9504aaf9caa829e3a1d23668018443bb
Nash Villarta <>
2021-05-05 17:18:51 +0000

    ''

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('button').addEventListener('click',randomizer)
+document.getElementById('button').addEventListener('click', randomizer)
 
 let randomNumber = 0
 let userGuess = 0
@@ -10,16 +10,16 @@ function randomizer () {
   randomNumber = (Math.random() * 6) + 1
   randomNumber = parseInt(randomNumber)
   document.getElementById('realnumber').innerHTML = randomNumber
-  if (userGuess == randomNumber) {
-    document.getElementById('windisplay').innerHTML = "Nice, that is the correct guess! Play again?"
+  if (userGuess === randomNumber) {
+    document.getElementById('windisplay').innerHTML = 'Nice, that is the correct guess! Play again?'
   }
   if (userGuess < randomNumber) {
-    document.getElementById('windisplay').innerHTML = "Too low! Try again!"
+    document.getElementById('windisplay').innerHTML = 'Too low! Try again!'
   }
   if (userGuess > randomNumber) {
-    document.getElementById('windisplay').innerHTML = "Too high! Try again!"
+    document.getElementById('windisplay').innerHTML = 'Too high! Try again!'
   }
   if (userGuess > 6) {
-    document.getElementById('windisplay').innerHTML = "Ranges from 1-6, remember that!"
+    document.getElementById('windisplay').innerHTML = 'Ranges from 1-6, remember that!'
   }
 }
